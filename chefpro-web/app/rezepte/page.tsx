d0980760004ef5b6cedfa6723490bfb318091f1f
frankcrivellari <frankcrivellari@icloud.com>
2026-02-02 20:24:36 +0100

    Debug: Add TEST header to rezepte page

--- chefpro-web/app/rezepte/page.tsx
+++ chefpro-web/app/rezepte/page.tsx
@@ -1,6 +1,11 @@
 import { RecipeEditor } from "@/components/recipes/recipe-editor";
 
 export default function RezeptePage() {
-  return <RecipeEditor mode="recipes" />;
+  return (
+    <>
+      <h1 style={{fontSize: '100px', color: 'purple', position: 'fixed', top: 0, left: 0, zIndex: 99999}}>TEST</h1>
+      <RecipeEditor mode="recipes" />
+    </>
+  );
 }
 
